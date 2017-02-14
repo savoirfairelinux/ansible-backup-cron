c67f04cc0a49eddf0c3758ba85b8615491d4570f
Virgil Dupras <virgil.dupras@savoirfairelinux.com>
2017-02-14 14:26:48 -0500

    Simplify our backup/restore procedure
    We now have a the same situation at backup and restore time: a pwd to a
    tmp folder with all items to backup/restore. This makes it much more
    convenient to add our new feature: the possibility to execute arbitrary
    commands at backup/restore time (such as dumping/restoring a DB).

--- templates/restore.sh
+++ templates/restore.sh
@@ -4,15 +4,26 @@
 # It will **overwrite** persistent data of this project with the ones in the latest backup
 # Do not run if the current persistent that is not already backuped up or non-existent!
 
-{% for folder in backup_cron_folders %}
-{{ backup_cron_storage_restore_script_path|mandatory }} | tar -x --wildcards --preserve-permissions --same-owner -C "{{ folder|dirname }}" "{{ folder|basename }}/*"
-{% endfor %}
-
 WORKDIR=`mktemp -d`
 cd $WORKDIR
 
-{% for folder in backup_cron_backup_script %}
-{{ folder.restore_command }}
+# First of all, let's delete our backed up folders. We do this before extracting in case we're low
+# on disk space.
+{% for folder in backup_cron_folders %}
+rm -rf "{{ folder }}"
+{% endfor %}
+
+# Now, lets extract the thing
+{{ backup_cron_storage_restore_script_path|mandatory }} | tar -x
+
+# Now, let's move folder back in position
+{% for folder in backup_cron_folders %}
+mv "{{ folder|basename }}" "{{ folder }}"
+{% endfor %}
+
+# And finally, we run restore commands
+{% for command in backup_cron_restore_commands %}
+{{ command }}
 {% endfor %}
 
 cd /tmp
