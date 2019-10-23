9e8cca56946e8f0f8e0a0cb79311c3039041cda7
Pierre Duchemin <pierre.duchemin@savoirfairelinux.com>
2019-10-23 12:29:28 -0400

    fix: avoid taking too much disk space

--- templates/backup.sh
+++ templates/backup.sh
@@ -10,6 +10,10 @@ TIMESTAMP=`date '+%Y%m%d%H%M%S'`
 
 DUMP_PATH={% if not backup_cron_custom_tmp_folder %}"."{% else %}"{{ backup_cron_custom_tmp_folder }}"{% endif %}
 
+# Delete oldest backup
+rm "$(ls -t $DUMP_PATH/*.sql.gz | tail -1)"
+rm "$(ls -t $DUMP_PATH/*.tar.gz | tail -1)"
+
 {{ backup_cron_dbdump_script }} > $DUMP_PATH/database-backup-$TIMESTAMP.sql
 gzip $DUMP_PATH/database-backup-$TIMESTAMP.sql
 
@@ -18,5 +22,3 @@ gzip $DUMP_PATH/database-backup-$TIMESTAMP.sql
 # the --same-owner flag, tar begins with the chown, and then oops! it doesn't have permissions
 # for chgroup!
 tar --owner="{{ backup_cron_user }}" -chzf $DUMP_PATH/files-backup-$TIMESTAMP.tar.gz $TO_BACKUP
-
-find $DUMP_PATH -mtime +2 delete
