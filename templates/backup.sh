27c47dc82ec4fdedb7fa005eb5dbe72e83d3e199
Pierre Duchemin <pierre.duchemin@savoirfairelinux.com>
2019-10-23 11:53:44 -0400

    fix: change backup script

--- templates/backup.sh
+++ templates/backup.sh
@@ -1,38 +1,22 @@
 #!/bin/bash
-
-{% if not backup_cron_custom_tmp_folder|length %}
-WORKDIR=`mktemp -d`
-{% else %}
-mkdir -p {{ backup_cron_custom_tmp_folder }}
-WORKDIR={{ backup_cron_custom_tmp_folder }}
-{% endif %}
-cd $WORKDIR
-
-# In the tar command below, we can't use "*" to tell it to tar everything because it's going to
-# ignore hidden folders. Explicitly listing our subitems is the simplest idea we could come up
-# with.
-TO_BACKUP=""
+set -x
 
 {% for folder in backup_cron_folders %}
 ln -s "{{ folder }}" .
-TO_BACKUP="${TO_BACKUP} {{ folder|basename }}"
+TO_BACKUP="$TO_BACKUP {{ folder|basename }}" # list of files to backup
 {% endfor %}
 
-DUMP_PATH={% if not backup_cron_custom_tmp_folder %} "dump.sql" {% else %} "{{ backup_cron_custom_tmp_folder }}" {% endif %}
+TIMESTAMP=`date '+%Y%m%d%H%M%S'`
 
-{% if backup_cron_dbdump_script %}
-{{ backup_cron_dbdump_script }} > $DUMP_PATH/dump.sql
-TO_BACKUP="${$DUMP_PATH} ${TO_BACKUP}"
-{% endif %}
+DUMP_PATH={% if not backup_cron_custom_tmp_folder %}"."{% else %}"{{ backup_cron_custom_tmp_folder }}"{% endif %}
+
+{{ backup_cron_dbdump_script }} > $DUMP_PATH/database-backup-$TIMESTAMP.sql
+gzip $DUMP_PATH/database-backup-$TIMESTAMP.sql
 
 # Here, we force the owner of every file we add to be our backup_cron_user because for group
 # and permissions restoration to work on the other end, we need it to be so. Otherwise, with
 # the --same-owner flag, tar begins with the chown, and then oops! it doesn't have permissions
 # for chgroup!
-tar -ch --owner="{{ backup_cron_user }}" ${TO_BACKUP} | {{ backup_cron_storage_dump_script_path|mandatory }}
+tar --owner="{{ backup_cron_user }}" -chzf $DUMP_PATH/files-backup-$TIMESTAMP.tar.gz $TO_BACKUP
 
-{% if not backup_cron_custom_tmp_folder|length %}
-cd /tmp
-{% endif %}
-rm -rf "${WORKDIR}"
-
+find $DUMP_PATH -mtime +2 delete
--- templates/restore.sh
+++ templates/restore.sh
@@ -8,9 +8,9 @@
 {{ backup_cron_storage_restore_script_path|mandatory }} | tar -x --wildcards -C "{{ folder|dirname }}" "{{ folder|basename }}/*"
 {% endfor %}
 
-DUMP_PATH={% if not backup_cron_custom_tmp_folder %} "dump.sql" {% else %} "{{ backup_cron_custom_tmp_folder }}" {% endif %}
+DUMP_PATH={% if not backup_cron_custom_tmp_folder %}"."{% else %}"{{ backup_cron_custom_tmp_folder }}"{% endif %}
 
 {% if backup_cron_dbrestore_script %}
-{{ backup_cron_storage_restore_script_path|mandatory }} | tar -x --to-stdout ${DUMP_PATH} | {{ backup_cron_dbrestore_script }}
+{{ backup_cron_storage_restore_script_path|mandatory }} | tar -x --to-stdout $DUMP_PATH | {{ backup_cron_dbrestore_script }}
 {% endif %}
 
